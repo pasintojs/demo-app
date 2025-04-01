2917289c0f2b61f6aceaafda6ae8e460061f3455
pasintojs <pasinto.js@gmail.com>
2025-04-02 06:39:50 +0700

    saveour commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello Oh god</h1>
+      <h1 className="my-8 text-center text-5xl font-bold">
+        This update is from home
+      </h1>
     </>
   );
 }
