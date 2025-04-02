1ce063acdea4ff8c190194e6f0b4c18da0cf8327
pasintojs <pasinto.js@gmail.com>
2025-04-02 09:03:11 +0700

    committt

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <h1 className="my-8 text-center text-5xl font-bold">
-        This update is from Office 2/4/2568
+        This update is from Office 2/4/2568 8.59
       </h1>
     </>
   );
