eac70d7ec22d4da67dc219084abd525adfa9b9fd
pasintojs <pasinto.js@gmail.com>
2025-04-03 06:18:40 +0700

    add component

---
+++ src/components/ProjectsSidebar.jsx
@@ -0,0 +1,11 @@
+export default function ProjectsSidebar() {
+  return (
+    <>
+      <h2>Your Project</h2>
+      <div>
+        <button>Please add your project</button>
+      </div>
+      <ul></ul>
+    </>
+  );
+}
