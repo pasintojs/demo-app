4a103dbf6faecf29a2c3dfb5754ebc789e79cd79
pasintojs <pasinto.js@gmail.com>
2025-04-03 06:31:40 +0700

    OK

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,9 +1,16 @@
+import React from "react";
+
 export default function ProjectsSidebar() {
+  const [testValue, setTestValue] = React.useState(5);
+
+  function clickHandler() {
+    setTestValue((prev) => prev + 5);
+  }
   return (
     <aside>
-      <h2>Your Project</h2>
+      <h2>Your Project {testValue}</h2>
       <div>
-        <button>Please add your project</button>
+        <button onClick={clickHandler}>Please add your project</button>
       </div>
       <ul></ul>
     </aside>
