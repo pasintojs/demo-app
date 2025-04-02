e79d7a3728e0774e6e076c8bd9dd8837059a5051
pasintojs <pasinto.js@gmail.com>
2025-04-03 06:22:45 +0700

    -

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import ProjectsSidebar from "./components/ProjectsSidebar";
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">
-        This update is from Office 2/4/2568 8.59
-      </h1>
-    </>
+    <main>
+      <ProjectsSidebar />
+    </main>
   );
 }
 
--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,11 +1,11 @@
 export default function ProjectsSidebar() {
   return (
-    <>
+    <aside>
       <h2>Your Project</h2>
       <div>
         <button>Please add your project</button>
       </div>
       <ul></ul>
-    </>
+    </aside>
   );
 }
