cd0d83a79893fa8ba30dd84f5a9ad74ec5c699af
pasintojs <pasinto.js@gmail.com>
2025-04-07 19:44:22 +0700

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 function Navbar() {
   return (
-    <div className="bg-gray-300 max-w-screen-xl mx-auto text-white px-10 py-2 my-2 rounded flex items-center justify-between">
-      <div>Menu</div>
+    <div className="container mx-auto text-white bg-orange-500 rounded-2xl   py-2 my-2  flex items-center justify-between">
+      <div className="mx-3">Menu</div>
       <div>
-        <ul className="flex flex-col md:flex-row">
+        <ul className="flex flex-col md:flex-row mx-3">
           <li className="mx-5">test1</li>
           <li className="mx-5">test2</li>
           <li className="mx-5">test1</li>
